d1a78a1ea38a042a7d052132acb78befa36ce52b
wangxinyu00/xinhyu5.1 <1115276108@qq.com>
2019-06-18 19:17:36 +0800

    rikao9

---
+++ rikao9.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lenovo
+ * Date: 2019/6/18
+ * Time: 19:12
+ */
+
+function PrintMinNumber()
+{
+    $arr = [3,32,321];
+
+    var_dump($arr);
+}
+print_r(PrintMinNumber());
